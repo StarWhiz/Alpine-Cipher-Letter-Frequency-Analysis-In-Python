fb7b401af9ef109fcc3694618f074940cc73baa3
Tai <taidao90@gmail.com>
2019-03-02 18:16:46 -0800

    Used regex to filter input to all lowercase letters

--- tai_hw3/tai_hw3.py
+++ tai_hw3/tai_hw3.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_value_from_letter(letter):
     return {
         'a': 0,
@@ -59,9 +62,6 @@ def get_letter_from_value(number):
         25: 'z'
     }[number]
 
-def get_frequency_conversion():
-    print("hello")
-
 
 def affine_tester(message):
     """
@@ -79,19 +79,13 @@ def affine_tester(message):
 
 def message_formatter(string):
     """
-    This function returns a formatted string where it becomes all lowercase without any whitespaces in between.
-    Also removes periods, commas, question marks, and commas.
+    This function returns a formatted string where it becomes all lowercase without any other characters in between.
 
     :param string:
     :return string_to_return:
     """
-
+    string = re.sub('[^0-9a-zA-Z]+', '', string)
     string_to_return = string.lower()
-    string_to_return = string_to_return.replace(" ", "")
-    string_to_return = string_to_return.replace(".", "")
-    string_to_return = string_to_return.replace("?", "")
-    string_to_return = string_to_return.replace("!", "")
-    string_to_return = string_to_return.replace(",", "")
     return string_to_return
 
 
@@ -110,11 +104,28 @@ def affine_cipher(a, b, message):
     print(ciphertext)
     return ciphertext
 
+
 def letter_frequency_analysis(ciphertext):
-    print("placeholder")
+    get_letter_count(ciphertext)
 
 
-affine_tester("hello world how are you doing. This is Tai. He is the beast.")
+def get_letter_count(message):
+    letter_count = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+    for letter in message:
+        letter_count[letter] += 1
+    print(letter_count)
+
+
+
+a = r"""
+Swift is a fantastic way to write software, whether its for phones, desktops, servers, or anything else that runs code. Its a safe, fast, and interactive programming language that combines the best in modern language thinking with wisdom from the wider Apple engineering culture and the diverse contributions from its open-source community. The compiler is optimized for performance and the language is optimized for development, without compromising on either.
+
+Swift is friendly to new programmers. Its an industrial-quality programming language thats as expressive and enjoyable as a scripting language. Writing Swift code in a playground lets you experiment with code and see the results immediately, without the overhead of building and running an app.
+
+Swift defines away large classes of common programming errors by adopting modern programming pattern.
+"""
+
+affine_tester(a)
 
 
 
